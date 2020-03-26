0e5411f0968fc3d20e4989e1f4c3ccf490b592a6
Jami Kousa <jami.kousa@helsinki.fi>
2020-03-26 18:52:55 +0200

    forgot from setup

--- setup.js
+++ setup.js
@@ -2,7 +2,7 @@ const path = require('path')
 const spawn = require('child_process').spawn
 
 // Installs node modules for each
-const services = ['root', 'jami']
+const services = ['root', 'jami', 'tuomo', 'mikko', 'terho']
 
 services.forEach(service => {
   const appPath = path.join(__dirname, service)
